610b4da6fc309e19dbb348fff38fcb4985f2e021
Jorge Pedro López <salubri1@hotmail.com>
2025-11-27 20:57:31 +0100

    Implement various sorting functions for array
    Added sorting functions to demonstrate array manipulation.

--- 29_arrayordena.php
+++ 29_arrayordena.php
@@ -13,10 +13,32 @@
         var_dump($dias);
         echo "<br>";
 
+        asort ($dias);
+        echo "<b>asort ordena el array manteniendo las keys</b>";
+        var_dump($dias);
+        echo "<br>";
+
+        ksort ($dias);
+        echo "<b>ksort ordena las keys</b>";
+        var_dump($dias);
+        echo "<br>";
+
         sort ($dias);
+        echo "<b>sort ordena el array sin mantener las keys</b>";
+        var_dump($dias);
+        echo "<br>";
+
+        rsort ($dias);
+        echo "<b>rsort ordena el array en orden inverso sin mantener las keys</b>";
+        var_dump($dias);
+        echo "<br>";
+
+        shuffle($dias);
+        echo "<b>shuffle baraja el array</b>";
         var_dump($dias);
         echo "<br>";
         
+
     ?>
 </body>
 </html>
